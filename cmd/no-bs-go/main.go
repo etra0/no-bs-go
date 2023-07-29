c59d481d0966aab02827c5dcd7eaf7ffe8f1403d
Sebastián Aedo <me@etra0.lat>
2023-07-28 22:48:06 -0400

    add pprof

--- cmd/no-bs-go/main.go
+++ cmd/no-bs-go/main.go
@@ -4,11 +4,19 @@ import (
 	"log"
 	"os"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	internal_bot "github.com/etra0/no-bs-go/internal"
 	tbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
+	go func() {
+		log.Println("Starting pprof server on localhost:6060")
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	bot, err := tbot.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Fatal("Something went wrong: ", err)
